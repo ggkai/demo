c11df893d96fde182a649f3f07be208b53ec90d1
ggkai <411307280@qq.com>
2018-10-26 16:27:27 +0800

    new demo:command_line_RL

--- cv/人脸识别/getimgdata.py
+++ cv/人脸识别/getimgdata.py
@@ -3,83 +3,78 @@ import re
 import cv2
 import numpy as np
 
-
 class GetImgData:
     '''
     将图片数据处理成ndarray数据
     '''
-    # 初始化函数
-    def __init__(self,dir='.data/small_img_gray'):
+    def __init__(self,dir='./data/small_img_gray'):
         self.dir=dir # 该文件夹中有多个子文件夹，每个子文件夹名为一个人名，里面是这个人的多张人脸照片
 
-    # 第一步：传入路径，返回文件路径数组、文件数组
-    def get_file_name(path=None):
+    def get_file_path(self,path=None):
+        '''
+        获得文件中的所有图片路径
+        :param path: 文件路径
+        :return: 返回图片路径
+        '''
         filepaths = []
         filenames = os.listdir(path)  # 获取path中的所有文件名
         for filename in filenames:
-            if filename=='.DS_Store':
-                os.remove(path+'/'+filename) #去掉.DS_Store文件
-            filepath=os.path.join(path,filename) #获得文件路径
-            filepaths.append(filepath)
-        return filepaths,filenames
+            if re.findall('\d+\.jpg',filename)!=[]:
+                filepath=os.path.join(path,filename) #获得文件路径
+                filepaths.append(filepath) #添加到数组
+        return filepaths
 
-
-    # 第二步：传入文件数组，返回文件分类的onehot编码
-    def get_onehot(filenames):
-        onehot = np.zeros([len(filenames), len(filenames) ])  # 构建全零数组，行数为数字个数，列为数字最大值加一
-        onehot[np.arange(len(filenames)), np.arange(len(filenames))] = 1         # 利用数组的索引方式进行赋值，构建onehot编码
+    def get_onehot(self,index):
+        '''
+        传入文件数组，返回文件分类的onehot编码
+        :param filenames: 文件数组
+        :return: onehot编码数组
+        '''
+        num=len(index)
+        onehot = np.zeros([num,num])  # 构建全零数组，行数为数字个数，列为数字最大值加一
+        onehot[np.arange(num), np.arange(num)] = 1         # 利用数组的索引方式进行赋值，构建onehot编码
         return onehot
 
+    def get_file_label(self):
+        '''
+        获得self.dir中每个文件路径、onehot编码、数字标签
+        :return: 返回两个值
+        path_onehot: 返回list，文件路径和onehot编码，比如('./data/small_img_gray/hebo',[1,0,0,0,0])
+        num_label: 返回字典，key:数字标签，values:文件名，比如{0:'hebo',1:'hexianbin'}
+        '''
+        file_index=[] #[0,1,2,3,4,5,...]
+        file_name=[] #[hebo,hexianbin,...]
+        path_list=[] #['./data/hebo,'./data/hexianbin',...]
+        for index,item in enumerate(os.listdir(self.dir)):
+            file_index.append(index)
+            file_name.append(item)
+            path_list.append(os.path.join(self.dir,item))
+        filepath_onehot=list(zip(path_list,self.get_onehot(file_index)))
+        num_label=dict(zip(file_index,file_name))
+        return filepath_onehot,num_label
 
-    # 第三步：传入文件数组，返回分类名、分类标签
-    '''
-    def get_file_label(filenames):
-        label_index=[]
-        label_name=[]
-        for index,item in enumerate(filenames):
-            label_index.append(index)
-            label_name.append(item)
-        print(label_index,label_name)
-    '''
-    def get_file_label(filenames):
-        return np.arange(len(filenames))
+    def read_img(self):
+        '''
+        读取self.dir中所有图片，将图片转为数组，获得图片标签
+        :return: 返回三个值
+        x: 返回数组，图片像素数据
+        y: 返回list，图片标签onehot编码，比如(0,1,0,0,...)
+        num_label: 返回字典，图片数字标签和文件名，比如{0:'hebo',1:'hexianbin',...}
+        '''
+        imgs = [] #图片数组
+        labels = [] #图片标签[...]
 
-
-    # 第四步：传入路径，返回文件下图片路径、onehot编码、分类标签
-    # def get_img_onehot_label(path):
-    # #     dir_labels=list(zip(pathlist,b.tolist())) #人名路径：分类的独热编码
-    # #     number_name=dict(zip(indexlist,namelist)) #数字标签：分类
-
-    #     imgs = []     #图片像素数据
-    #     labels = []   #图片标签
-    #     for dirname,label in dir_labels:            #获得人名路径：分类独热编码
-    #         print(dirname,label)
-    #         for imgname in os.listdir(dirname):      #获得人名路径下的所有图片
-    #             print(imgname)
-    #             img = cv2.imread(dirname+'/'+imgname,cv2.IMREAD_GRAYSCALE)  #读取图片(灰度图)
-    #             imgs.append(img)                  #存贮图片像素数据
-    #             labels.append(label)              #存贮图片标签数据
-    #             x = np.array(imgs,dtype='float32')/255    #将图片像素数据转为数组并归一化
-    #              = np.array(labels,dtype='float32')      #将标签数据转为数组
-    #             print(x,y,labels_name)
-    def readimg(path):
-        imgs = []
-        onehots = []
-        labels = []
-
-        filepaths, filenames = get_file_name(path)
-        label = get_file_label(filenames)
-        onehot = get_onehot(filenames)
-
-        i = 0
-        for filepath in filepaths:  # 获得路径
-            imgpaths, imgnames = get_file_name(filepath)  # 获得图片
-            for imgpath in imgpaths:
-                img = cv2.imread(imgpath)
+        filepath_onehot,num_label=self.get_file_label()
+        for filepath,onehot in filepath_onehot:
+            for imgpath in self.get_file_path(filepath):
+                img=cv2.imread(imgpath,cv2.IMREAD_GRAYSCALE)
                 imgs.append(img)
-                onehots.append(onehot[i])
-                labels.append(label[i])
-            i += 1
-        x = np.array(imgs, dtype='float32') / 255
-        y = np.array(labels, dtype='float32')
-        return x, y, labels
+                labels.append(onehot)
+        x=np.array(imgs,dtype='float32')/255
+        y=np.array(labels,dtype='float32')
+        return x,y,num_label
+
+if __name__ == '__main__':
+    imgData = GetImgData()
+    x,y,name = imgData.read_img()
+    print(x.shape,y.shape,name)
