5445b5d6049b4d3e04bfc00bee64f15578c02fe7
ggkai <411307280@qq.com>
2018-12-11 14:29:50 +0800

    fix

--- NLP/EntityRecognition.py
+++ NLP/EntityRecognition.py
@@ -1,31 +1,26 @@
 #stanfordcorenlp
+'''
+NE类型支持：位置、人物、组织、货币、百分比、日期、时间
+'''
 from stanfordcorenlp import StanfordCoreNLP
 
 sentence = '清华大学位于北京。'
-
-with StanfordCoreNLP(r'G:\JavaLibraries\stanford-corenlp-full-2018-02-27', lang='zh') as nlp:
+with StanfordCoreNLP('/Users/guokai/Downloads/stanford-corenlp-full-2018-02-27', lang='zh') as nlp:
     print(nlp.word_tokenize(sentence))
     print(nlp.pos_tag(sentence))
     print(nlp.ner(sentence))
     print(nlp.parse(sentence))
     print(nlp.dependency_parse(sentence))
 
-#
-
-
 #pyltp
-import os
-LTP_DATA_DIR = '/path/to/your/ltp_data'  # ltp模型目录的路径
-ner_model_path = os.path.join(LTP_DATA_DIR, 'ner.model')  # 命名实体识别模型路径，模型名称为`pos.model`
-
+'''
+NE类型支持：人名、地名、机构名
+'''
 from pyltp import NamedEntityRecognizer
 recognizer = NamedEntityRecognizer() # 初始化实例
-recognizer.load(ner_model_path)  # 加载模型
-
+recognizer.load('/Users/guokai/Desktop/ltp_data_v3.4.0/ner.model')  # 加载模型
 words = ['元芳', '你', '怎么', '看']
 postags = ['nh', 'r', 'r', 'v']
-netags = recognizer.recognize(words, postags)  # 命名实体识别
-
-print ('\t'.join(netags))
+nertags = recognizer.recognize(words, postags)  # 命名实体识别
+print ('\t'.join(netrags))
 recognizer.release()  # 释放模型
-
